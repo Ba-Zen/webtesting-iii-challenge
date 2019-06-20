8d38ba89ea8e03c9ac8cee03c8b35b80b57eda0f
Ba-Zen <bazenatlas@gmail.com>
2019-06-19 22:09:39 -0400

    mvp

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,10 @@
-// Test away
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import Dashboard from './Dashboard';
+
+describe('<Dashboard />', () => {
+  it('should render the dashboard', () => {
+    render(<Dashboard />);
+  });
+});
